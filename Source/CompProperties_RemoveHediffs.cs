92eaae7c8406e3befba07a822127d9500a10e754
Aelanna <aelanna@gmail.com>
2024-05-04 15:32:56 -0500

    Updated for RimWorld 1.5

--- Source/CompProperties_RemoveHediffs.cs
+++ Source/CompProperties_RemoveHediffs.cs
@@ -1,33 +1,33 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 using RimWorld;
 using Verse;
 
 namespace ExampleMedicalItem
 {
-    // This is the custom CompProperties used to configure this effect in XML
-    public class CompProperties_RemoveHediffs : CompProperties_UseEffect
+  // This is the custom CompProperties used to configure this effect in XML
+  public class CompProperties_RemoveHediffs : CompProperties_UseEffect
+  {
+    // List of HediffDefs that we want this medicine to remove
+    public List<HediffDef> hediffsRemoved;
+
+    // List of HediffDefs that this medicine causes as a side effect
+    public List<HediffDef> sideEffects;
+
+    public CompProperties_RemoveHediffs()
     {
-        // List of HediffDefs that we want this medicine to remove
-        public List<HediffDef> hediffsRemoved;
-
-        // List of HediffDefs that this medicine causes as a side effect
-        public List<HediffDef> sideEffects;
-
-        public CompProperties_RemoveHediffs()
-        {
-            // Specifying the compClass in the constructor ensures that the correct ThingComp subclass is invoked
-            compClass = typeof(CompUseEffect_RemoveHediffs);
-        }
-
-        public override IEnumerable<string> ConfigErrors(ThingDef parentDef)
-        {
-            // Throw a configuration error on game load if no HediffDefs were specified for purging
-            if (hediffsRemoved.NullOrEmpty())
-            {
-                // Error messages usually don't need to be localized as they are meant to inform developers, not players
-                yield return $"(Example Medical Item) {parentDef.defName} has CompProperties_RemoveHediffs, but is not configured to remove any hediffs.";
-            }
-        }
+      // Specifying the compClass in the constructor ensures that the correct ThingComp subclass is invoked
+      compClass = typeof(CompUseEffect_RemoveHediffs);
     }
+
+    public override IEnumerable<string> ConfigErrors(ThingDef parentDef)
+    {
+      // Throw a configuration error on game load if no HediffDefs were specified for purging
+      if (hediffsRemoved.NullOrEmpty())
+      {
+        // Error messages usually don't need to be localized as they are meant to inform developers, not players
+        yield return $"(Example Medical Item) {parentDef.defName} has CompProperties_RemoveHediffs, but is not configured to remove any hediffs.";
+      }
+    }
+  }
 }
--- Source/CompUseEffect_RemoveHediffs.cs
+++ Source/CompUseEffect_RemoveHediffs.cs
@@ -1,65 +1,65 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 using RimWorld;
 using Verse;
 
 namespace ExampleMedicalItem
 {
-    // This is the ThingComp subclass that is invoked when the parent item is used
-    public class CompUseEffect_RemoveHediffs : CompUseEffect
+  // This is the ThingComp subclass that is invoked when the parent item is used
+  public class CompUseEffect_RemoveHediffs : CompUseEffect
+  {
+
+    // This helper property lets us read the configuration
+    public CompProperties_RemoveHediffs Props => (CompProperties_RemoveHediffs)props;
+
+    // A predicate method to help filter hediffs on the user
+    public bool IsRemovableHediff(Hediff hediff)
     {
-
-        // This helper property lets us read the configuration
-        public CompProperties_RemoveHediffs Props => (CompProperties_RemoveHediffs)props;
-
-        // A predicate method to help filter hediffs on the user
-        public bool IsRemovableHediff(Hediff hediff)
-        {
-            return Props.hediffsRemoved.Contains(hediff.def);
-        }
-
-        // DoEffect is called when the item is used
-        public override void DoEffect(Pawn usedBy)
-        {
-            base.DoEffect(usedBy);
-
-            // Find list of removable hediffs
-            List<Hediff> hediffs = new List<Hediff>();
-            usedBy.health.hediffSet.GetHediffs(ref hediffs, IsRemovableHediff);
-
-            if (hediffs.Count > 0)
-            {
-                // Remove each hediff from the user
-                foreach (Hediff hediff in hediffs)
-                {
-                    usedBy.health.RemoveHediff(hediff);
-                }
-
-                // Notify the player of how many hediffs were removed
-                Messages.Message(
-                    text: "ExampleMedicalItem_CountConditionsRemoved".Translate(parent.LabelCapNoCount, hediffs.Count, usedBy.Name.ToStringShort),
-                    def: MessageTypeDefOf.TaskCompletion,
-                    historical: false
-                );
-            }
-            else
-            {
-                // Notify the player that no hediffs were removed
-                Messages.Message(
-                    text: "ExampleMedicalItem_NoConditionsRemoved".Translate(parent.LabelCap, usedBy.Name.ToStringShort),
-                    def: MessageTypeDefOf.TaskCompletion,
-                    historical: false
-                );
-            }
-
-            // Add each side effect to the user
-            if (Props.sideEffects != null)
-            {
-                foreach(HediffDef def in Props.sideEffects)
-                {
-                    usedBy.health.AddHediff(def);
-                }
-            }
-        }
+      return Props.hediffsRemoved.Contains(hediff.def);
     }
+
+    // DoEffect is called when the item is used
+    public override void DoEffect(Pawn usedBy)
+    {
+      base.DoEffect(usedBy);
+
+      // Find list of removable hediffs
+      List<Hediff> hediffs = new List<Hediff>();
+      usedBy.health.hediffSet.GetHediffs(ref hediffs, IsRemovableHediff);
+
+      if (hediffs.Count > 0)
+      {
+        // Remove each hediff from the user
+        foreach (Hediff hediff in hediffs)
+        {
+          usedBy.health.RemoveHediff(hediff);
+        }
+
+        // Notify the player of how many hediffs were removed
+        Messages.Message(
+            text: "ExampleMedicalItem_CountConditionsRemoved".Translate(parent.LabelCapNoCount, hediffs.Count, usedBy.Name.ToStringShort),
+            def: MessageTypeDefOf.TaskCompletion,
+            historical: false
+        );
+      }
+      else
+      {
+        // Notify the player that no hediffs were removed
+        Messages.Message(
+            text: "ExampleMedicalItem_NoConditionsRemoved".Translate(parent.LabelCap, usedBy.Name.ToStringShort),
+            def: MessageTypeDefOf.TaskCompletion,
+            historical: false
+        );
+      }
+
+      // Add each side effect to the user
+      if (Props.sideEffects != null)
+      {
+        foreach (HediffDef def in Props.sideEffects)
+        {
+          usedBy.health.AddHediff(def);
+        }
+      }
+    }
+  }
 }
